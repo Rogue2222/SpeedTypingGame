9e9180bf7da92a593c4cd4ed034c542aa4971b2d
Remarci225 <71138785+Remarci225@users.noreply.github.com>
2024-12-16 21:47:06 +0100

    Updated build script

--- upload.cmd
+++ upload.cmd
@@ -1,19 +1,16 @@
 :: The following upload script assumes that you have butler installed (https://itch.io/docs/butler/)
 :: and have a Builds directory at the same level as the project directory with the following structure:
 :: 	Builds/
+	  - WebGL.zip
 :: 	  - Windows/
 ::	      - Windows.zip
 ::	  - macOS/
 ::	      - SpeedTypingGame.app.zip
-::	  - Linux/
-::	      - Linux.zip
-::	  - WebGL.zip
 
 @echo off
 
 set target="remarci/speedtypinggame"
 
+butler push ..\Builds\WebGL.zip %target%:webgl --userversion 1.0.0
 butler push ..\Builds\Windows\Windows.zip %target%:windows --userversion 1.0.0
 butler push ..\Builds\macOS\SpeedTypingGame.app.zip %target%:macos --userversion 1.0.0
-butler push ..\Builds\Linux\Linux.zip %target%:linux --userversion 1.0.0
-butler push ..\Builds\WebGL.zip %target%:webgl --userversion 1.0.0
